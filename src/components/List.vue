a6e65de69b9e9d60c98d29f561bf293c11ac7d05
jimao <shihao.gu@perfma.com>
2024-10-09 18:23:09 +0800

    基本对话框

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const modal = ref<any>(null);
+</script>
+
+<template>
+  <div :style="{ width: '800px' }">
+    <div>
+      <a-button type="primary" @click="modal = { }">新增</a-button>
+    </div>
+    <div>
+      <a-table />
+    </div>
+  </div>
+  <a-modal
+    :title="modal?.uid ? '编辑项目' : '新增项目'"
+    v-model:open="modal">
+  </a-modal>
+</template>
